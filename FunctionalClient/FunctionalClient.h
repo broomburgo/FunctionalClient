b9fe4c62ffc32be664c0ffd1666a1281b8224ab8
Elviro Rocca <elviro.rocca@facile.it>
2015-05-20 09:11:58 +0200

    Initial Commit

---
+++ FunctionalClient/FunctionalClient.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalClient.h
+//  FunctionalClient
+//
+//  Created by Elviro Rocca on 20/05/15.
+//  Copyright (c) 2015 Elvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalClient.
+FOUNDATION_EXPORT double FunctionalClientVersionNumber;
+
+//! Project version string for FunctionalClient.
+FOUNDATION_EXPORT const unsigned char FunctionalClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalClient/PublicHeader.h>
+
+
